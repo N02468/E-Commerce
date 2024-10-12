7ebc17483bee855f29ee423fa1934796b70e869a
N02468 <mn3110403@gmail.com>
2024-10-12 14:11:15 +0500

    Third Commit

---
+++ src/components/payment.jsx
@@ -0,0 +1,13 @@
+// src/components/Payment.jsx
+import React from 'react';
+
+const Payment = () => {
+    return (
+        <div>
+            <h1>Payment Page</h1>
+            {/* Add your payment logic here */}
+        </div>
+    );
+};
+
+export default Payment;
